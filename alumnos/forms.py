4582d5182c0f1de5649cada3f4f077954d876a28
Dave <alexruedarivas@outlook.com>
2020-06-16 21:13:57 -0500

    Post sin funcionar todo lo demás bien

--- alumnos/forms.py
+++ alumnos/forms.py
@@ -4,14 +4,6 @@ from .models import Alumno, Soa, Programacion_web
 
     
     
-class SoaForm(forms.ModelForm):
-    class Meta:
-        model = Soa
-        fields = [
-            "unit1",
-            "unit2",
-            "unit3"
-        ]
 
 class AlumnoForm(forms.ModelForm):
     class Meta:
--- alumnos/views.py
+++ alumnos/views.py
@@ -2,29 +2,39 @@ from django.shortcuts import render, get_object_or_404
 from django.template import loader
 from django.http import HttpResponse
 
-from .forms import AlumnoForm, SoaForm
-from .models import Alumno
+from .forms import AlumnoForm
+from .models import Alumno, Soa, Programacion_web
 
 # Create your views here.
 
 
 # def create_alumno(request):
-#     context = {}
+#     data = request.POST
+
+#     soaInit = Soa(unit1=data.get("a_u1"), unit2=data.get(
+#         "a_u2"), unit3=data.get("a_u3"))
+
+#     soaInit.save()
+#     print("test",soaInit)
+
+#     pwebInit = Programacion_web(unit1=data.get(
+#         "p_u1"), unit2=data.get("p_u2"), unit3=data.get("p_u3"))
+
+#     form = Alumno(name=data.get("name"), last_name=data.get(
+#         "last_name"), pweb=pwebInit, arqui=soaInit)
+
+
+#     context = {"form": form}
 #     return render(request, "alumnos/alumno_create.html", context)
 
 
 def create_alumno(request):
     form = AlumnoForm(request.POST or None)
-    soa = SoaForm(request.POST or None)
     if form.is_valid():
         form.save()
-        
-    if soa.is_valid():
-        soa.save()
 
     context = {
         "form": form,
-        "soa": soa
     }
 
     return render(request, "alumnos/alumno_create.html", context)
